d615da56992fd33462f77974d36e2e3303723c2a
Frank Mueller <frank.mueller@themue.dev>
2022-02-26 23:15:27 +0100

    Add test with higher burst

--- throttle_test.go
+++ throttle_test.go
@@ -26,7 +26,7 @@ import (
 // TESTS
 //--------------------
 
-// TestThrottleOK verifies the positiv limitation of parallel processed events.
+// TestThrottleOK verifies the positiv throttling of parallel processed events.
 func TestThrottleOK(t *testing.T) {
 	assert := asserts.NewTesting(t, asserts.FailStop)
 	runs := 110
@@ -62,6 +62,55 @@ func TestThrottleOK(t *testing.T) {
 
 		assert.Equal(l, wait.Limit(20.0))
 		assert.Equal(b, 1)
+		assert.Logf("RUN: %d / LIMIT: %.2f / BURST: %d", rs, l, b)
+
+		if rs >= int64(runs) {
+			break
+		}
+	}
+
+	duration := time.Now().Sub(start)
+
+	assert.Equal(rushes, int64(runs))
+	assert.True(duration.Seconds() >= 5.0, "duration is", duration.String())
+}
+
+// TestThrottleBurstOK verifies the positiv throttling of parallel processed events with burst.
+func TestThrottleBurstOK(t *testing.T) {
+	assert := asserts.NewTesting(t, asserts.FailStop)
+	runs := 110
+	var rushes int64
+	event := func() error {
+		atomic.AddInt64(&rushes, 1)
+		return nil
+	}
+	throttle := wait.NewThrottle(20.0, 5)
+	rushing := func() {
+		rush := func() {
+			ctx := context.Background()
+			throttle.Process(ctx, event, event, event, event, event)
+		}
+		for i := 0; i < runs/5; i++ {
+			go rush()
+		}
+	}
+	// All preparations done, now start rushing and check the load.
+	start := time.Now()
+
+	go rushing()
+
+	ticker := time.NewTicker(5 * time.Millisecond)
+	defer ticker.Stop()
+
+	for {
+		<-ticker.C
+
+		rs := atomic.LoadInt64(&rushes)
+		l := throttle.Limit()
+		b := throttle.Burst()
+
+		assert.Equal(l, wait.Limit(20.0))
+		assert.Equal(b, 5)
 		assert.Logf("RUN: %d / LIMIT: %.2f / BURST: %d", rs, l, b)
 
 		if rs >= int64(runs) {
